0e43e1a857b1123d19851ed3732ef4caa347a36e
Ari <gryphon150@gmail.com>
2019-10-28 16:48:57 -0700

    added first prop

--- src/MyList.jsx
+++ src/MyList.jsx
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ListItem from './ListItem';
 
+class MyList extends Component {
+  render() {
+    return(
+      <div>
+        <h1> Things I should stop procrastinating: </h1>
+        <ul>
+          <ListItem doThis="Buy Ice-cream"/>
+        </ul>
+      </div>
+    )
+  }
 
+  
 
-export default App;
+}
+
+export default MyList;
