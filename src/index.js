92756aaa7f9e8abf9f123405215c622fa8fbb910
Ari <gryphon150@gmail.com>
2019-10-28 16:25:25 -0700

    basic set up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyList from './MyList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
